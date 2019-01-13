3a37660ff21065cf3db4fd3e1bd20b9fc66754d7
John Ennis <johnennis5@gmail.com>
2019-01-13 17:07:02 +0000

    Added dimension and custom reduce function
    dappDim to pluck platfrom type
    
    averageUsersPerPlatformGroup custom reduce to get average users

--- static/js/graph.js
+++ static/js/graph.js
@@ -15,4 +15,37 @@ function makeGraphs(error, dappsData) {
         d.txs_24hr = +d.txs_24hr;
     });
 
+    // calling chart functions
+    show_user_per_platform_average(ndx);
+
+    dc.renderAll();
+
+}
+
+function show_user_per_platform_average(ndx) {
+
+    var dappDim = ndx.dimension(dc.pluck("platform"));
+
+    var averageUsersPerPlatformGroup = dappDim.group().reduce(
+
+        function(p, v) {
+            p.count++;
+            p.total += v.users_24hr;
+            return p;
+        },
+
+        function(p, v) {
+            p.count--;
+            if (p.count == 0) {
+                p.total = 0;
+            } else {
+                p.total -= v.users_24hr;
+            }
+            return p;
+        },
+
+        function() {
+            return { count: 0, total: 0 };
+        }
+    );
 }
