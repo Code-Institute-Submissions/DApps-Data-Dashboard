39f121857916ede55160c2b3ea014d1aaa4f3041
John Ennis <johnennis5@gmail.com>
2019-01-26 15:11:49 +0000

    Updated graph.js, graphSpec.js to single quotes

--- Unit-Tests/spec/graphSpec.js
+++ Unit-Tests/spec/graphSpec.js
@@ -1,9 +1,9 @@
-describe("Chart Tests", function() {
+describe('Chart Tests', function() {
   var ndx = crossfilter();
 
   // Build pieChart1
   function buildPieChart(id) {
-    var pieChart1 = dc.pieChart("#" + id);
+    var pieChart1 = dc.pieChart('#' + id);
     pieChart1
       .dimension(dappDim)
       .group(averageUsersPerPlatform)
@@ -15,7 +15,7 @@ describe("Chart Tests", function() {
     return pieChart1;
   }
 
-  var dappDim = ndx.dimension(dc.pluck("platform"));
+  var dappDim = ndx.dimension(dc.pluck('platform'));
   var averageUsersPerPlatform = dappDim.group().reduce(
     function(p, v) {
       p.count++;
@@ -36,43 +36,43 @@ describe("Chart Tests", function() {
     }
   );
 
-  describe("pieChart1", function() {
+  describe('pieChart1', function() {
     var pieChart1;
 
     beforeEach(function() {
-      pieChart1 = buildPieChart("users-balance");
+      pieChart1 = buildPieChart('users-balance');
       pieChart1.dimension();
       pieChart1.render();
     });
 
-    it("should exist", function() {
+    it('should exist', function() {
       expect(show_user_per_platform_average(ndx)).not.toBeNull();
     });
 
-    it("should have chart", function() {
+    it('should have chart', function() {
       expect(dc.hasChart(pieChart1)).toBeTruthy();
     });
 
-    it("should have a width", function() {
+    it('should have a width', function() {
       expect(pieChart1.width()).toEqual(200);
     });
 
-    it("should have a height", function() {
+    it('should have a height', function() {
       expect(pieChart1.height()).toEqual(200);
     });
 
-    it("should have an innerRadius", function() {
+    it('should have an innerRadius', function() {
       expect(pieChart1.innerRadius()).toEqual(30);
     });
 
-    it("should be responsive", function() {
+    it('should be responsive', function() {
       expect(pieChart1.useViewBoxResizing(true)).toBeTruthy();
     });
   });
 
   // build barchart
   function buildBarChart(id) {
-    var barChart = dc.barChart("#" + id);
+    var barChart = dc.barChart('#' + id);
     barChart
       .dimension(categoryDim)
       .group(categoryGroup)
@@ -83,39 +83,39 @@ describe("Chart Tests", function() {
     return barChart;
   }
 
-  var categoryDim = ndx.dimension(dc.pluck("category"));
-  var categoryGroup = categoryDim.group().reduceSum(dc.pluck("users_24hr"));
+  var categoryDim = ndx.dimension(dc.pluck('category'));
+  var categoryGroup = categoryDim.group().reduceSum(dc.pluck('users_24hr'));
 
-  describe("barChart", function() {
+  describe('barChart', function() {
     var barChart;
 
     beforeEach(function() {
-      barChart = buildBarChart("category-balance");
+      barChart = buildBarChart('category-balance');
       barChart.dimension();
       barChart.render();
     });
 
-    it("should exist", function() {
+    it('should exist', function() {
       expect(show_categories_user_balance(ndx)).not.toBeNull();
     });
 
-    it("should have chart", function() {
+    it('should have chart', function() {
       expect(dc.hasChart(barChart)).toBeTruthy();
     });
 
-    it("should have a width", function() {
+    it('should have a width', function() {
       expect(barChart.width()).toEqual(600);
     });
 
-    it("should have a height", function() {
+    it('should have a height', function() {
       expect(barChart.height()).toEqual(300);
     });
 
-    it("should have an ordinal scale", function() {
+    it('should have an ordinal scale', function() {
       expect(barChart.x(d3.scale.ordinal())).toBeTruthy();
     });
 
-    it("should be responsive", function() {
+    it('should be responsive', function() {
       expect(barChart.useViewBoxResizing(true)).toBeTruthy();
     });
   });
