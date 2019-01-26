4ec4eb560b5c6a76ca90e9708ccbfb9a7bb9477b
John Ennis <johnennis5@gmail.com>
2019-01-26 13:36:49 +0000

    added 4 more tests to pieChart1
    hasChart Test
    width Test
    Height Test
    Is responsive Test

--- Unit-Tests/spec/graphSpec.js
+++ Unit-Tests/spec/graphSpec.js
@@ -1,8 +1,24 @@
-describe('Chart Tests', function () {
+describe('Chart Tests', function() {
     var ndx = crossfilter();
-    describe('pieChart1', function () {
-        it('should exist', function () {
+    describe('pieChart1', function() {
+        it('should exist', function() {
             expect(show_user_per_platform_average(ndx)).not.toBeNull();
-        })
-    })
+        });
+
+        it('should have chart', function() {
+            expect(dc.pieChart("#users-balance")).toBeTruthy();
+        });
+
+        it('should have a width', function() {
+            expect(dc.pieChart("#users-balance").width()).toEqual(200);
+        });
+
+        it('should have a height', function() {
+            expect(dc.pieChart("#users-balance").height()).toEqual(200);
+        });
+
+        it('should be responsive', function() {
+            expect(dc.pieChart("#users-balance").useViewBoxResizing(true)).toBeTruthy();
+        });
+    });
 });
